aa10dcce83ec2f5f0fa321feef7be8abc17dd293
Venkat Ragavan <mail2venkat21@gmail.com>
2021-02-12 22:13:24 +0530

    RN skeleton setup

--- App.js
+++ App.js
@@ -1,20 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
+import React,{Component} from 'react';
+import { StyleSheet, Text, View, Dimensions} from 'react-native';
+import AnimatedSplash from "react-native-animated-splash-screen";
+import * as Notifications from 'expo-notifications';
+import Constants from 'expo-constants';
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
+export default class App extends Component{
+  state = {
+    isLoaded: false,
+  }
+  async componentDidMount(){
+    setTimeout( () => {
+      this.setState({ isLoaded: true })
+    },2000);
+  }
+  render(){
+    return(
+      <AnimatedSplash
+        translucent={true}
+        isLoaded={this.state.isLoaded}
+        logoImage={require("./assets/splash.png")}
+        backgroundColor={"black"}
+        logoHeight={windowHeight}
+        logoWidth={windowWidth}
+      >
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
-  );
+    </AnimatedSplash>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
