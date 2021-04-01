6c184076f0a6304232e7b6b8c1f09f2aba949983
Venkat Ragavan <mail2venkat21@gmail.com>
2021-04-02 00:31:13 +0530

    Added WebView Google Data Studio

--- screens/AnalyticsScreen.js
+++ screens/AnalyticsScreen.js
@@ -1,6 +1,7 @@
 import React,{Component} from 'react';
 import { StyleSheet, Text, View, Dimensions, Image, ScrollView} from 'react-native';
 import HeaderComponent from '../components/HeaderComponent';
+import { WebView } from 'react-native-webview';
 const windowWidth = Dimensions.get('window').width;
 export default class RenderAnalytics extends Component{
   state = {
@@ -15,22 +16,9 @@ export default class RenderAnalytics extends Component{
       </View>
       <View style={styles.textView}> 
         <Text style={styles.textFont}>Analytics Screen</Text>
-        <ScrollView style={styles.scrollView}>
-          <Image
-          style={styles.logo}
-          source={{
-            uri: 'https://reactnative.dev/img/tiny_logo.png',
-          }}
-          />
-          <Text style={styles.textFont}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-            ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-            ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
-            sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-            est laborum.
-          </Text>
-        </ScrollView>
+        <View style={{flex:1}}> 
+        <WebView source={{ uri:'https://datastudio.google.com/embed/u/0/reporting/f9915176-47e8-4025-b2b1-68f8c02246b3/page/MKtAC' }} style={{ flex:1 }} />
+        </View>
       </View>
     </View>
     )
@@ -42,29 +30,17 @@ const styles = StyleSheet.create({
     flex: 1,
     width: windowWidth,
   },
-  textScroll: {
-    fontSize: 30,
-  },
   header: {
     width: '100%',
     flex: 0.1,
     alignItems: 'center',
     justifyContent: 'center',
   },
-  home: {
-    width: '100%',
-    flex: 1,
-  },
-  logo: {
-    width: 300,
-    height: 300,
-  },
   textView:{
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
   },
   textFont: {
+    alignSelf: 'center',
     fontSize: 30,
   }
 });
